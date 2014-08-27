081a4e0e647a0a465b211c1bc7ef19828daf27b1
Gregory Haskins <gregory.haskins@gmail.com>
2014-08-26 22:53:02 -0400

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- main.cc
+++ main.cc
@@ -1,10 +1,12 @@
-
+#include <queue>
 
 #include <boost/filesystem.hpp>
 #include <boost/regex.hpp>
 #include <boost/program_options.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/future.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/uuid/sha1.hpp>
 #include <boost/iostreams/device/file.hpp>
@@ -13,6 +15,66 @@ namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
 typedef std::string Sha;
+typedef boost::packaged_task<Sha> Task;
+typedef boost::shared_ptr<Task> TaskPtr;
+
+class ThreadPool
+{
+public:
+  ThreadPool()
+  {
+    int concurrency(boost::thread::hardware_concurrency());
+    std::cerr << "Using " << concurrency << " threads" << std::endl;
+
+    for (int i(0); i<concurrency; i++)
+      {
+	m_tg.create_thread(boost::bind(&ThreadPool::worker, this));
+      }
+  }
+
+  ~ThreadPool()
+  {
+    m_tg.interrupt_all();
+    m_tg.join_all();
+  }
+
+  void post(TaskPtr task)
+  {
+    Lock lock(m_mutex);
+
+    m_tasks.push(task);
+    m_condition.notify_one();
+  }
+  
+private:
+  void worker()
+  {
+    while (1)
+      {
+	Lock lock(m_mutex);
+	
+	while(m_tasks.empty())
+	  m_condition.wait(lock);
+
+	TaskPtr task(m_tasks.front());
+	m_tasks.pop();
+
+	(*task)();
+      }
+  }
+
+  typedef boost::mutex              Mutex;
+  typedef boost::unique_lock<Mutex> Lock;
+  typedef boost::condition_variable Condition;
+  typedef std::queue<TaskPtr>          Tasks;
+
+  boost::thread_group m_tg;
+  Mutex               m_mutex;
+  Condition           m_condition;
+  Tasks               m_tasks;
+};
+
+ThreadPool _pool;
 
 class File : public fs::path
 {
@@ -20,25 +82,29 @@ public:
 
   File(fs::path path) : fs::path(path)
   {
-    m_future = boost::async(boost::launch::deferred,
-			    [path]()->Sha
-			    {
-			      using boost::uuids::detail::sha1;
-
-			      boost::iostreams::file_source is(path.string());
-			      sha1                          sha1;
-			      char                          buf[4096];
-			      std::streamsize               len;
-
-			      while ((len = is.read(buf, sizeof(buf))))
-				sha1.process_bytes(buf, len);
-			      
-			      unsigned int digest[5];
-
-			      sha1.get_digest(digest);
-
-			      return "sha";
-			    });
+    TaskPtr task(new Task([path]()->Sha
+			  {
+			    using boost::uuids::detail::sha1;
+			    
+			    boost::iostreams::file_source is(path.string());
+			    sha1                          sha1;
+			    char                          buf[4096];
+			    std::streamsize               len;
+			    
+			    //while ((len = is.read(buf, sizeof(buf))))
+			    //sha1.process_bytes(buf, len);
+			    
+			    unsigned int digest[5];
+			    
+			    sha1.get_digest(digest);
+			    
+			    return "sha";
+			  }
+			  )
+		 );
+		 
+    m_future = task->get_future();
+    _pool.post(task);
   }
 
   Sha sha()
@@ -52,7 +118,7 @@ public:
 
 private:
   typedef boost::unique_future<Sha> Future;
-  
+
   Future m_future;
 };
 
@@ -65,7 +131,7 @@ public:
       {
 	fs::directory_entry entry(*iter);
 	fs::path p(entry.path());
-	
+
 	if (p.filename() == ".verisum")
 	  continue;
 
@@ -93,7 +159,7 @@ public:
   {
     for (auto iter : m_files)
       {
-	
+
       }
 
     for (auto iter : m_subdirs)
@@ -113,7 +179,7 @@ int main(int argc, char **argv)
   int ret;
   fs::path path(fs::current_path());
   bool generate(false);
-  
+
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help,h", "produces help message")
@@ -121,10 +187,10 @@ int main(int argc, char **argv)
      "path to tree (defaults to $CWD)")
     ("generate", "generate sums")
     ;
-  
+
   po::positional_options_description p;
   p.add("path", -1);
-  
+
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
   po::notify(vm);
